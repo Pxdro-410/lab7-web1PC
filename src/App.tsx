7246917e0e5cce3c4fb9fb5f075d37db2f644e1f
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-05-21 20:55:55 -0600

    fix: correccion en la estructura para cumplir con el requisito de -20 lineas

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import './App.css'
 import { Calculator } from './components/Calculator'
+import { Credentials } from './components/Credentials'
 import { MathBackground } from './components/MathBackground'
 
 function App() {
   return (
     <>
       <MathBackground />
-      <div className="credentials">
-        <h3>lab 7 web</h3>
-        <h4>autor:</h4>
-        <p>Pedro Caso</p>
-        <h4>carnet:</h4>
-        <p>241286</p>
-      </div>
+      <Credentials />
       <Calculator />
     </>
   )
