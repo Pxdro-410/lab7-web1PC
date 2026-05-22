d36894c9c72ce5e8545186f805883b57389ca0a0
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-05-21 21:29:53 -0600

    feat: añadir credenciales al proyecto

---
+++ src/components/Credentials.tsx
@@ -0,0 +1,13 @@
+import './Credentials.css'
+
+export function Credentials() {
+  return (
+    <div className="credentials">
+      <h3>lab 7 web</h3>
+      <h4>autor:</h4>
+      <p>Pedro Caso</p>
+      <h4>carnet:</h4>
+      <p>241286</p>
+    </div>
+  )
+}
